ffbe56f1bcd8a789cacf6d7ddb9dde974129b31a
jadehare <guo15838596879@live.com>
2025-08-04 22:05:31 +0800

    添加跳跃判断

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -7,10 +7,21 @@ public class PlayerBehavior : MonoBehaviour
 
     public float MoveSpeed = 10f;
     public float RotationSpeed = 75f;
+
+    public float JumpVelocity = 5f;
+
+    public float DistanceToGround = 0.1f;
+
+    public LayerMask GroudLayer;
+
+    private CapsuleCollider _col;
+
     private Rigidbody _rb;
 
     private float _vInput;
     private float _hInput;
+
+    private bool _isJumping = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +30,12 @@ public class PlayerBehavior : MonoBehaviour
         if (_rb == null)
         {
             Debug.LogError("Rigidbody component not found on PlayerBehavior script.");
+        }
+
+        _col = GetComponent<CapsuleCollider>();
+        if (_col == null)
+        {
+            Debug.LogError("CapsuleCollider component not found on PlayerBehavior script.");
         }
     }
 
@@ -32,6 +49,9 @@ public class PlayerBehavior : MonoBehaviour
 
         // transform.Translate(Vector3.forward * _vInput * Time.deltaTime);
         // transform.Rotate(Vector3.up * _hInput * Time.deltaTime);
+
+        //
+        _isJumping |= Input.GetKeyDown(KeyCode.J);
 
     }
 
@@ -47,5 +67,21 @@ public class PlayerBehavior : MonoBehaviour
 
         Quaternion rotation = Quaternion.Euler(0, _hInput * Time.deltaTime, 0);
         _rb.MoveRotation(_rb.rotation * rotation);
+        // 物理检测上一致，放在fixedupdate中判断。
+        bool isGrounded = IsGrounded();
+        if (isGrounded && _isJumping)
+        {
+            _rb.AddForce(Vector3.up * JumpVelocity, ForceMode.Impulse);
+        }
+        _isJumping = false;
+
+    }
+
+    private bool IsGrounded()
+    {
+        Vector3 capsuleBottom = new Vector3(_col.bounds.center.x, _col.bounds.min.y, _col.bounds.center.z);
+        // 忽略Trigger碰撞器
+        bool grounded = Physics.CheckCapsule(_col.bounds.center, capsuleBottom, DistanceToGround, GroudLayer, QueryTriggerInteraction.Ignore);
+        return grounded;
     }
 }
