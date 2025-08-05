f7cdfcbdbeb57f4f0a99e95e0dbb971d08e9cfca
jadehare <guo15838596879@live.com>
2025-08-06 00:20:38 +0800

    添加ai navigation， 添加ai移动。

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -1,19 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    public Transform PatrolRoute;
+
+    public List<Transform> Locations;
+
+    private int _locationIndex = 0;
+
+    private NavMeshAgent _agent;
+
     // Start is called before the first frame update
     void Start()
     {
+        _agent = GetComponent<NavMeshAgent>();
+        InitializePatrolRoute();
+        MoveToNextPatrolLocation();
+    }
 
+    void InitializePatrolRoute()
+    { 
+        foreach (Transform child in PatrolRoute)
+        {
+            Locations.Add(child);
+        }
+    }
+
+    void MoveToNextPatrolLocation()
+    {
+        _agent.destination = Locations[_locationIndex].position;
+        _locationIndex = (_locationIndex + 1) % Locations.Count;
+        Debug.Log("Moving to next patrol location: " + Locations[_locationIndex].name);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (_agent.remainingDistance < 0.2f && !_agent.pathPending)
+        {
+            MoveToNextPatrolLocation();
+        }
     }
 
     void OnTriggerEnter(Collider other)
